24f89c18b9a06895e0ee8f661ab2e4d8d2cafbb1
TsuShinra <70816820+TsuShinra@users.noreply.github.com>
2020-10-04 01:37:19 +0300

    init: Typical Array Problems - done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+  if (array === [] || array == 0 || array == undefined) {return 0;}
+  else {
+    array.sort( (a, b) => a - b );
+    return array[0];
+  }
   return 0;
-}
+};
 
 exports.max = function max (array) {
+  if (array === [] || array == 0 || array == undefined) {return 0;}
+  else {
+    array.sort( (a, b) => a - b );
+    return array[(array.length - 1)];
+  }
   return 0;
-}
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let result = 0;
+  if ( array == 0 || array == undefined) {return result;}
+  if (array !== []) {
+    for (let i=0; i < array.length; i+=1) {
+      result = result + array[i];
+    }
+    result = result / array.length;
+  }
+  return result;
+};
